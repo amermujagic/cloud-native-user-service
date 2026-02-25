e35ea74ebc98edf7519fc64af796fdbda11a57d0
amer.mujagic <amer.mujagic@endava.com>
2026-02-25 12:30:26 +0100

    Use Docker Hub image instead of local one

--- app/src/server.js
+++ app/src/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "User Service Running",
+    message: "User Service Running v3",
     environment: APP_ENV,
     dbPasswordConfigured: DB_PASSWORD !== "not-set"
   });
